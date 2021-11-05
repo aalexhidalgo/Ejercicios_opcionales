6ef646d0aefa3da5b9e2a07e2e2a413a5235c9ed
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2021-11-05 18:42:06 +0100

    Ejercicio Opcional 1

---
+++ Ejercicio Opcional 1/Assets/Scripts/ParImpar.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParImpar : MonoBehaviour
+{
+    private int num;
+
+    public void EsParImpar(int num)
+    {
+        if (num % 2 == 0)
+        {
+            Debug.Log($"El número {num} es par.");
+        }
+        else
+        {
+            Debug.Log($"El número {num} es impar.");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            num = int.Parse(GetComponent<InputField>().text);
+            EsParImpar(num);
+        }
+    }
+}
