ae6c917eff000acaf8a33811cd50dc47f3e10862
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2021-12-13 02:03:15 +0100

    E02

--- Ejercicio Opcional 2/Assets/Scripts/ColorManager.cs
+++ Ejercicio Opcional 2/Assets/Scripts/ColorManager.cs
@@ -4,28 +4,25 @@ using UnityEngine;
 
 public class ColorManager : MonoBehaviour
 {
-    public Color Cyan()
+    public Color RandomColor()
     {
-        return new Color(0f, 1f, 1f);
+        return new Color(Random.Range(0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<MeshRenderer>().material.color = Cyan();
+        GetComponent<MeshRenderer>().material.color = RandomColor();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ()
-        {
-            OnMouseDown();
-        }
+        
     }
 
     private void OnMouseDown()
     {
-        GetComponent<Camera>().backgroundColor = Cyan();
+        Camera.main.GetComponent<Camera>().backgroundColor = RandomColor();
     }
 }
