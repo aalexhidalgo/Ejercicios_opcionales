d5aa7adf06ff3d5bbe3299ab1714ea5bc9ec6027
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2021-12-13 23:02:39 +0100

    E02

--- Ejercicio Opcional 2/Assets/Scripts/ColorManager.cs
+++ Ejercicio Opcional 2/Assets/Scripts/ColorManager.cs
@@ -9,9 +9,11 @@ public class ColorManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Accedemos al material, al color
         GetComponent<MeshRenderer>().material.color = MyColor;
     }
 
+    //Cuando presionemos encima del material se transformará el color del fondo con el color del material
     private void OnMouseDown()
     {
         Camera.main.GetComponent<Camera>().backgroundColor = MyColor;
